3a9750b8b645eb5cf395e5bf5d047ab6302f2c06
sheepers <sheepersinc@gmail.com>
2013-04-06 19:16:23 +0300

    12

--- grails-app/controllers/gsandbox/RegisterController.groovy
+++ grails-app/controllers/gsandbox/RegisterController.groovy
@@ -1,4 +1,12 @@
 package gsandbox
 
 class RegisterController extends grails.plugins.springsecurity.ui.RegisterController {
+
+
+    def index{
+        //redirect uri: defaultTargetUrl
+
+    }
+
+
 }
